b1dc5c7ff6c1b1b2a95bb668b0d4b510f1ab8349
denonia <denoniaa@gmail.com>
2024-07-19 21:50:50 +0300

    Serving avatars

---
+++ src/Camel.Bancho/Controllers/AvatarController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Camel.Bancho.Controllers;
+
+[Host("a.camel.local")]
+public class AvatarController : ControllerBase
+{
+    private readonly IConfiguration _configuration;
+
+    public AvatarController(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+    
+    // TODO: this should probably go to nginx
+    [HttpGet("/{userId}")]
+    public IActionResult Avatar(int userId)
+    {
+        var dataDir = _configuration.GetRequiredSection("DataDir").Value;
+        var path = Path.Combine(Path.GetFullPath(dataDir), "a", userId + ".jpg");
+
+        if (!System.IO.File.Exists(path))
+            return NotFound();
+
+        var fs = new FileStream(path, FileMode.Open);
+        
+        return File(fs, "image/jpeg");
+    }
+}
